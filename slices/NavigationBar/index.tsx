06625a5676dae84ad0ed5492408ace2b0716d27c
kad1kad <k.aliu@me.com>
2025-05-09 15:56:37 +0100

    feat: add default variation for navigation bar slice

--- slices/NavigationBar/index.tsx
+++ slices/NavigationBar/index.tsx
@@ -2,6 +2,76 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
+// {
+//     "variation": "default",
+//     "version": "initial",
+//     "items": [],
+//     "primary": {
+//         "logo": {},
+//         "menu_items": [
+//             {
+//                 "label": "Product",
+//                 "has_dropdown": true,
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "dropdown_id": "product"
+//             },
+//             {
+//                 "label": "Pricing",
+//                 "has_dropdown": null,
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "dropdown_id": "pricing"
+//             },
+//             {
+//                 "label": "Case Studies",
+//                 "has_dropdown": null,
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "dropdown_id": "case_studies"
+//             },
+//             {
+//                 "label": "Resources",
+//                 "has_dropdown": true,
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "dropdown_id": "resources"
+//             },
+//             {
+//                 "label": "Company",
+//                 "has_dropdown": true,
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "dropdown_id": "company"
+//             }
+//         ],
+//         "action_buttons": [
+//             {
+//                 "label": "Login",
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "style": "secondary"
+//             },
+//             {
+//                 "label": "Book a demo",
+//                 "url": {
+//                     "link_type": "Any"
+//                 },
+//                 "style": "primary"
+//             }
+//         ]
+//     },
+//     "id": "navigation_bar$91423edf-e1a6-4617-a5be-a245ec484ae6",
+//     "slice_type": "navigation_bar",
+//     "slice_label": null
+// }
+
 /**
  * Props for `NavigationBar`.
  */
